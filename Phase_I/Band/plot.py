5551e25a788af942e072c8a8eea7dc396ec6f4b2
Pacidus <pacidus@gmail.com>
2022-12-14 04:15:35 +0100

    Somme significatives amelioration of the band structure plot

--- Phase_I/Band/plot.py
+++ Phase_I/Band/plot.py
@@ -6,4 +6,6 @@ fermi = read_fermi("Ammonia.out")
 bands, x = read_bnd("AC_bands.dat")
 
 plot(bands, x, fermi)
+plt.xticks(ticks= [0.0000, 0.5000, 1.2071, 1.5133, 2.0996], labels=['L', '$\Gamma$', 'X', 'U', '$\Gamma$'])
+plt.ylabel("Energy (eV)")
 plt.show()
